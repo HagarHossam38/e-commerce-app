f00dc34e8ae20dc8cc4996c3db31e319355913fa
HagarHossam38 <hagarhossam610@gmail.com>
2026-03-28 07:20:08 +0200

    git commit -m "handle prerender issue2"

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -4,7 +4,7 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
-  }, { path: '/product-details/:id', renderMode: RenderMode.Server },
-  { path: '/category-filter/:id', renderMode: RenderMode.Server },
-  { path: '/brands-filter/:id', renderMode: RenderMode.Server },
+  }, { path: 'product-details/:id', renderMode: RenderMode.Server },
+  { path: 'category-filter/:id', renderMode: RenderMode.Server },
+  { path: 'brands-filter/:id', renderMode: RenderMode.Server },
 ];

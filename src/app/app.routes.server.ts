955fb7e61c741bc50d33137fa087d2f7a8c546a2
HagarHossam38 <hagarhossam610@gmail.com>
2026-03-28 10:34:19 +0200

    update app.routes.server

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,7 +1,27 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  { path: 'product-details/:id', renderMode: RenderMode.Server },
+  { path: 'category-filter/:id', renderMode: RenderMode.Server },
+  { path: 'brands-filter/:id', renderMode: RenderMode.Server },
+  {
+    path: 'allorders',
+    renderMode: RenderMode.Server   // SSR
+  },
+  {
+    path: 'cart',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'checkout',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'wishlist',
+    renderMode: RenderMode.Server
+  },
+
   {
     path: '**',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Prerender
   },];

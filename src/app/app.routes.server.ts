297db3e59a2e5b838024131b1df1ddb0103c31c4
HagarHossam38 <hagarhossam610@gmail.com>
2026-03-28 07:13:35 +0200

    git commit -m "handle render issue"

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -4,5 +4,7 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
-  }
+  }, { path: '/product-details/:id', renderMode: RenderMode.Server },
+  { path: '/category-filter/:id', renderMode: RenderMode.Server },
+  { path: '/brands-filter/:id', renderMode: RenderMode.Server },
 ];
